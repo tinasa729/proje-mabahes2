cea2897c79200321cf62b856252f9bc706403942
tina <tinasa729@gmail.com>
2021-07-24 11:01:27 +0430

    Create ProductController.php

---
+++ project/app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::query();
+
+        if($keyword = request('search')) {
+            $products->where('title' , 'LIKE' , "%{$keyword}%")->orWhere('id' , 'LIKE' , "%{$keyword}%" );
+        }
+
+        $products = $products->latest()->paginate(20);
+        return view('admin.products.all' , compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'inventory' => 'required',
+            'categories' => 'required',
+            'image' =>'required'
+        ]);     
+
+        $product = auth()->user()->products()->create($validData);
+        $product->categories()->sync($validData['categories']);\
+
+        alert()->success('محصول مورد نظر با موفقیت ثبت شد' , 'با تشکر');
+        return redirect(route('admin.products.index'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return view('admin.products.edit' , compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'image' => 'required|max:2048',
+            'inventory' => 'required',
+            'categories' => 'required'
+        ]);
+
+        $product->update($validData);
+        $product->categories()->sync($validData['categories']);
+
+        alert()->success('محصول مورد نظر با موفقیت ویرایش شد' , 'با تشکر');
+        return redirect(route('admin.products.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        alert()->success('محصول مورد نظر با موفقیت حذف شد' , 'با تشکر');
+        return back();
+    }
+}
