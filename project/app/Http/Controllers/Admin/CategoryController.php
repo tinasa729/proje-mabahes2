96f216527d3eb95b23c2b7571f9b370b370387a9
tina <tinasa729@gmail.com>
2021-07-24 11:01:20 +0430

    Create CategoryController.php

---
+++ project/app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::query();
+        $categories = $categories->latest()->paginate(20);
+        return view('admin.categories.all' , compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3'
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+        ]);
+
+        alert()->success('دسته مورد نظر ثبت شد');
+        return redirect(route('admin.categories.index'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('admin.categories.edit' , compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|min:3'
+        ]);
+
+        $category->update([
+            'name' => $request->name,
+        ]);
+        alert()->success('دسته مورد نظر ویرایش شد');
+        return redirect(route('admin.categories.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        alert()->success('دسته مورد نظر حذف شد');
+        return back();
+    }
+}
