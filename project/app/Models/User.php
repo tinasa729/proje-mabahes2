0abbcbda05a8567ff8875499502a3fe95e1186e2
tina <tinasa729@gmail.com>
2021-07-24 11:03:45 +0430

    Create User.php

---
+++ project/app/Models/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+use App\Notifications\ResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_number'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public function isSuperUser()
+    {
+        return $this->is_superuser;
+    }
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function activecode(){
+        return $this->hasMany(ActiveCode::class);
+    }
+    public function requestbooks(){
+        return $this->hasMany(RequestBooks::class);
+    }
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+}
