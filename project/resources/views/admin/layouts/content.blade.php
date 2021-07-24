88f5b18d823ab8359c347b13debf46f86f04564a
tina <tinasa729@gmail.com>
2021-07-24 11:09:20 +0430

    Create content.blade.php

---
+++ project/resources/views/admin/layouts/content.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.master')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">{{ $title }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            {{ $slot }}
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
